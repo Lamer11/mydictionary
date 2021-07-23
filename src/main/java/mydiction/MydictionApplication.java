eec3c9e6000dce263879e3a4e8327ac1e23e702c
Lamer11 <emelya111@mail.ru>
2021-07-23 19:57:57 +0300

    init commit

---
+++ src/main/java/mydiction/MydictionApplication.java
@@ -0,0 +1,15 @@
+package mydiction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydictionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MydictionApplication.class, args);
+		
+		System.out.println("Тестовй лог 1111111111111111111111111");
+	}
+
+}
---
+++ src/test/java/mydiction/MydictionApplicationTests.java
@@ -0,0 +1,13 @@
+package mydiction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydictionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
